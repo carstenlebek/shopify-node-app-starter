f476acd0fc8eec27c1e67e71ca5f5675afe8635d
Jonathan Headen <jonathan.headen@outlook.com>
2022-01-20 19:02:56 +1100

    Add frame ancestors to the next config
    When publishing to shopify there is a requirement to protect the (iframe)[https://shopify.dev/apps/store/security/iframe-protection] by setting the frame ancestors.
    This config is discussed in this (slack thread)[https://shopifypartners.slack.com/archives/C4E5GV5U4/p1628099641042700]

--- next.config.js
+++ next.config.js
@@ -6,6 +6,34 @@ const envHost = JSON.stringify(process.env.HOST);
 const host = process.env.HOST;
 
 module.exports = {
+      async headers() {
+      return [
+        {
+          source: "/(.*?)",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-ancestors 'self' https://*.myshopify.com",
+            },
+          ],
+        },
+        {
+          source: "/(.*?)",
+          has: [
+            {
+              type: "query",
+              key: "shop",
+            },
+          ],
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-ancestors 'self' https://admin.shopify.com https://:shop;",
+            },
+          ],
+        },
+      ];
+    },
   webpack: (config) => {
     const env = { API_KEY: apiKey, HOST: envHost };
     config.plugins.push(new webpack.DefinePlugin(env));
