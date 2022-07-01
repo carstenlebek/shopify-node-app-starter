f2cfd04e94bb668136dc3f8ddf8fe07aa19145fd
carstenlebek <carsten.lebek@googlemail.com>
2022-07-01 02:14:18 +0200

    :heavy_minus_sign: @apollo/client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,15 @@
 import 'src/styles/globals.css';
 
-import {
-	ApolloClient,
-	ApolloProvider,
-	HttpLink,
-	InMemoryCache,
-} from '@apollo/client';
-
 import { AppBridgeProvider } from '@components/providers/AppBridgeProvider';
 import { GraphQLProvider } from '@components/providers/GraphQLProvider';
 import { PolarisProvider } from '@components/providers/PolarisProvider';
 
 function MyApp({ Component, pageProps }) {
-	const client = new ApolloClient({
-		cache: new InMemoryCache(),
-		link: new HttpLink({
-			uri: `/api/graphql`,
-			fetch: async (uri, options) => {
-				const response = await fetch(uri, {
-					redirect: 'follow',
-					...options,
-				});
-				if (response.redirected) {
-					window.location.href = response.url;
-				}
-				return response;
-			},
-		}),
-	});
-
 	return (
 		<PolarisProvider>
 			<AppBridgeProvider>
 				<GraphQLProvider>
-					<ApolloProvider client={client}>
-						<Component {...pageProps} />
-					</ApolloProvider>
+					<Component {...pageProps} />
 				</GraphQLProvider>
 			</AppBridgeProvider>
 		</PolarisProvider>
