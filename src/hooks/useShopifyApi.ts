b0ae763962460090bf9e5861de820d49c2ec3543
carstenlebek <carsten.lebek@googlemail.com>
2022-07-01 19:47:36 +0200

    ✨ custom hook for @graphql-codege

---
+++ src/hooks/useShopifyApi.ts
@@ -0,0 +1,24 @@
+import { GraphQLClient } from 'graphql-request';
+import { useAuthenticatedFetch } from './useAuthenticatedFetch';
+
+/**
+ * A hook for querying and mutating admin data.
+ * @desc A thin wrapper around `useAuthenticatedFetch`.
+ * It is configured to be passed to `@graphql-codegen` as a custom fetcher
+ * to generate hooks to make queries using the Shopify Admin GraphQL API.
+ * See: https://react-query.tanstack.com/reference/useQuery
+ *
+ * @returns {Array} An array containing the query data, loading state, and error state.
+ */
+export const useShopifyApi = <TData, TVariables>(
+	query: string
+): ((variables: TVariables) => Promise<TData>) => {
+	const authenticatedFetch = useAuthenticatedFetch();
+	const graphQLClient = new GraphQLClient('/api/graphql', {
+		fetch: authenticatedFetch,
+	});
+
+	return async (variables) => {
+		return (await graphQLClient.rawRequest(query, variables)).data;
+	};
+};
