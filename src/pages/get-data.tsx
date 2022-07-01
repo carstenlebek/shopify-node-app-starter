1b8a3e18e3976e8101e13c8783b12c0073da5443
carstenlebek <carsten.lebek@googlemail.com>
2022-07-01 14:58:24 +0200

    :sparkles: admin api example

--- src/pages/get-data.tsx
+++ src/pages/get-data.tsx
@@ -1,65 +1,77 @@
+import { Card, IndexTable, Layout, Page, TextStyle } from '@shopify/polaris';
+import { CurrencyCode, GetProductsQuery } from '@graphql/generated';
+
+import { GET_PRODUCTS } from '@graphql/queries/GET_PRODUCTS';
 import Link from 'next/link';
 import styles from 'src/styles/Global.module.css';
+import { useShopifyQuery } from 'src/hooks';
 import { useState } from 'react';
 
 export default function GetData() {
-	const [products, setProducts] = useState([]);
-
-	const [message, setMessage] = useState('');
+	const { data, isLoading } = useShopifyQuery<GetProductsQuery>(
+		'getProducts',
+		GET_PRODUCTS,
+		{
+			first: 10,
+		}
+	);
 
 	const getApi = async (e) => {
 		e.preventDefault();
 
 		await fetch('/api/hello', {
 			method: 'GET',
-		})
-			.then((r) => r.text())
-			.then((r) => setMessage(r));
+		}).then((r) => r.text());
 	};
 
+	const formatPrice = ({
+		amount,
+		currencyCode,
+	}: {
+		amount: any;
+		currencyCode: CurrencyCode;
+	}) =>
+		parseInt(amount).toLocaleString('en-US', {
+			style: 'currency',
+			currency: currencyCode,
+		});
+
+	const rowMarkup = data?.data.products.nodes.map(
+		(
+			{ id, title, priceRangeV2: { minVariantPrice, maxVariantPrice } },
+			index
+		) => (
+			<IndexTable.Row id={id} key={id} position={index}>
+				<IndexTable.Cell>
+					<TextStyle variation='strong'>{title}</TextStyle>
+				</IndexTable.Cell>
+				<IndexTable.Cell>
+					{formatPrice(minVariantPrice)}
+					{' - '}
+					{formatPrice(maxVariantPrice)}
+				</IndexTable.Cell>
+			</IndexTable.Row>
+		)
+	);
+
 	return (
-		<div className={styles.container}>
-			<Link href={'/'}>
-				<div className={styles.back}> &larr;</div>
-			</Link>
-			<main className={styles.main}>
-				<h1 className={styles.title}>Get Data</h1>
-				<p className={styles.description}>
-					View the examples in{' '}
-					<code className={styles.code}>pages/app/get-data.js</code>
-				</p>
-				<div>
-					<div className={styles.card}>
-						<h2>Get Data from Shopify&apos;s GraphQL Api</h2>
-						<button className={styles.button} onClick={() => {}}>
-							Fetch
-						</button>
-						{products.map((p) => {
-							return (
-								<div key={p.node.id} className={styles.listItem}>
-									<h2>{p.node.title}</h2>
-									<p>
-										{parseFloat(
-											p.node.priceRangeV2.minVariantPrice.amount
-										).toLocaleString('en-US', {
-											style: 'currency',
-											currency:
-												p.node.priceRangeV2.minVariantPrice.currencyCode,
-										})}
-									</p>
-								</div>
-							);
-						})}
-					</div>
-					<div className={styles.card}>
-						<h2>Get Data form your own Api</h2>
-						<button className={styles.button} onClick={getApi}>
-							Fetch
-						</button>
-						<h2>{message}</h2>
-					</div>
-				</div>
-			</main>
-		</div>
+		<Page title='Get data'>
+			<Layout>
+				<Layout.AnnotatedSection title='Get data from the Admin API'>
+					<Card>
+						<IndexTable
+							resourceName={{ singular: 'product', plural: 'products' }}
+							itemCount={data ? data.data.products.nodes.length : 0}
+							headings={[{ title: 'Name' }, { title: 'Price range' }]}
+							selectable={false}
+							loading={isLoading}
+						>
+							{rowMarkup}
+						</IndexTable>
+					</Card>
+				</Layout.AnnotatedSection>
+				<Layout.AnnotatedSection title='Get data from your own API'></Layout.AnnotatedSection>
+			</Layout>
+		</Page>
 	);
 }
