b31d3c39e9812bd495264b660763b6a11312f028
carstenlebek <carsten.lebek@googlemail.com>
2022-07-01 21:43:31 +0200

    🧹 cleanup

--- src/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ApiRequest, NextApiResponse } from "@types";
-
-export default function handler(req: ApiRequest, res: NextApiResponse) {
-  res.status(200).json("Hello There!")
-}
