1521d3fd0c5f7d35c43601f6d4ea05cfe6634ff9
carstenlebek <carsten.lebek@googlemail.com>
2022-07-01 02:08:37 +0200

    :sparkles: Next link component

--- src/components/providers/PolarisProvider.tsx
+++ src/components/providers/PolarisProvider.tsx
@@ -1,30 +1,26 @@
-import { useCallback } from "react";
-import { AppProvider } from "@shopify/polaris";
-import { useNavigate } from "@shopify/app-bridge-react";
-import translations from "@shopify/polaris/locales/en.json";
-import "@shopify/polaris/build/esm/styles.css";
+import '@shopify/polaris/build/esm/styles.css';
+
+import { AppProvider } from '@shopify/polaris';
+import Link from 'next/link';
+import translations from '@shopify/polaris/locales/en.json';
 
 function AppBridgeLink({ url, children, external, ...rest }) {
-  const navigate = useNavigate();
-  const handleClick = useCallback(() => {
-    navigate(url);
-  }, [url]);
 
-  const IS_EXTERNAL_LINK_REGEX = /^(?:[a-z][a-z\d+.-]*:|\/\/)/;
+	const IS_EXTERNAL_LINK_REGEX = /^(?:[a-z][a-z\d+.-]*:|\/\/)/;
 
-  if (external || IS_EXTERNAL_LINK_REGEX.test(url)) {
-    return (
-      <a target="_blank" rel="noopener noreferrer" href={url} {...rest}>
-        {children}
-      </a>
-    );
-  }
+	if (external || IS_EXTERNAL_LINK_REGEX.test(url)) {
+		return (
+			<a target='_blank' rel='noopener noreferrer' href={url} {...rest}>
+				{children}
+			</a>
+		);
+	}
 
-  return (
-    <a onClick={handleClick} {...rest}>
-      {children}
-    </a>
-  );
+	return (
+		<Link href={url} {...rest}>
+			<a {...rest}>{children}</a>
+		</Link>
+	);
 }
 
 /**
@@ -48,9 +44,9 @@ function AppBridgeLink({ url, children, external, ...rest }) {
  *
  */
 export function PolarisProvider({ children }) {
-  return (
-    <AppProvider i18n={translations} linkComponent={AppBridgeLink}>
-      {children}
-    </AppProvider>
-  );
+	return (
+		<AppProvider i18n={translations} linkComponent={AppBridgeLink}>
+			{children}
+		</AppProvider>
+	);
 }
