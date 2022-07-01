8814630aae858be8fcfa3dd96b7342d1f78f6945
carstenlebek <carsten.lebek@googlemail.com>
2022-07-01 19:58:07 +0200

    🧹 cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import 'src/styles/globals.css';
-
 import { AppBridgeProvider } from '@components/providers/AppBridgeProvider';
 import { GraphQLProvider } from '@components/providers/GraphQLProvider';
 import { PolarisProvider } from '@components/providers/PolarisProvider';
