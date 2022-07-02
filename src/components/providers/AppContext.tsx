8eab6ef93a99c72d00dfd9c77721469f5ae6eb6e
carstenlebek <carsten.lebek@googlemail.com>
2022-07-02 14:55:34 +0200

    Added app context

---
+++ src/components/providers/AppContext.tsx
@@ -0,0 +1,49 @@
+import { DisplayText, Spinner, Stack } from '@shopify/polaris';
+import { createContext, useContext } from 'react';
+
+import { OnlineAccessInfo } from '@lib/utils/loadCurrentSession';
+import { trpc } from '@lib/utils/trpc';
+
+const AppContext = createContext<{
+	user: Partial<OnlineAccessInfo['associated_user']> | undefined;
+}>({ user: {} });
+
+export const AppContextProvider = ({ children }) => {
+	const { data: currentUser, isLoading } = trpc.useQuery(['currentUser'], {
+		staleTime: 3000,
+	});
+
+	if (isLoading) {
+		return (
+			<div
+				style={{
+					height: '100vh',
+					width: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				<Stack
+					vertical
+					alignment='center'
+					distribution='center'
+					spacing='extraLoose'
+				>
+					<Spinner size='large' />
+					<DisplayText>App is loading...</DisplayText>
+				</Stack>
+			</div>
+		);
+	}
+
+	return (
+		<AppContext.Provider value={{ user: currentUser }}>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+export const useAppContext = () => {
+	return useContext(AppContext);
+};
