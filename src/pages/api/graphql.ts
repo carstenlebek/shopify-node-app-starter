ae0bd1ee7a8612b52feb2902b0bccc102197618d
carstenlebek <carsten.lebek@googlemail.com>
2022-07-02 14:51:21 +0200

    Content-Security-Policy Header

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,18 +1,27 @@
-import Shopify from "@lib/shopify";
-import { ApiRequest, NextApiResponse } from "@types";
+import { ApiRequest, NextApiResponse } from '@types';
+
+import Shopify from '@lib/shopify';
+import loadCurrentSession from '@shopify/shopify-api/dist/utils/load-current-session';
 
 export default async function handler(req: ApiRequest, res: NextApiResponse) {
-    try {
-        const response = await Shopify.Utils.graphqlProxy(req, res);
-        res.status(200).send(response.body);
-    } catch (err) {
-        console.error(err.message)
-        res.status(500).send(err.message);
-    }
+	const session = await loadCurrentSession(req, res, true);
+	try {
+		const response = await Shopify.Utils.graphqlProxy(req, res);
+		res
+			.status(200)
+			.setHeader(
+				'Content-Security-Policy',
+				`frame-ancestors https://${session?.shop} https://admin.shopify.com;`
+			)
+			.send(response.body);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send(err.message);
+	}
 }
 
 export const config = {
-    api: {
-        bodyParser: false,
-    },
-}
+	api: {
+		bodyParser: false,
+	},
+};
