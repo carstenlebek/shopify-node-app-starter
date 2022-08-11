58821a1bfa4ce825427ba1280f591ec629c9019d
carstenlebek <carsten.lebek@googlemail.com>
2022-08-11 10:53:24 +0200

    Added mandatory GDPR webhooks (https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks)

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -1,10 +1,28 @@
 const webhooks = {
-	APP_UNINSTALLED: {
-		path: '/api/webhooks',
-		webhookHandler: async (topic: string, shop: string, body: any) => {
-			console.log('App uninstalled');
-		},
-	},
+    "APP_UNINSTALLED": {
+        path: '/api/webhooks',
+        webhookHandler: async (topic: string, shop: string, body: any) => {
+            console.log('App uninstalled');
+        },
+    },
+    "CUSTOMERS_DATA_REQUEST": {
+        path: '/api/webhooks',
+        webhookHandler: async (topic: string, shop: string, body: any) => {
+            console.log('Customer data request');
+        }
+    },
+    "CUSTOMERS_REDACT": {
+        path: '/api/webhooks',
+        webhookHandler: async (topic: string, shop: string, body: any) => {
+            console.log('Customer data redacted');
+        }
+    },
+    "SHOP_REDACT": {
+        path: '/api/webhooks',
+        webhookHandler: async (topic: string, shop: string, body: any) => {
+            console.log('Shop data redacted');
+        }
+    }
 };
 
 export default webhooks;
